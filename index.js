ed53028ed0f58032f2bb3a5bf4f617ae396b603b
zavarykin <a.zavarykin@ase-ec.ru>
2025-06-06 13:45:42 +0300

    refactoring and add unit tests

--- index.js
+++ index.js
@@ -2,28 +2,19 @@ const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
-const fortune = require('./lib/fortune')
+const handler = require('./lib/handlers')
 
 app.engine('handlebars', expressHandlebars.engine({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handler.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', {fortune: fortune.getFortune})
-})
+app.get('/about', handler.about)
 
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handler.notFound)
 
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handler.serverError)
 
 app.listen(port, () => console.log(
     `Express запущен на http://localhost:${port}`
